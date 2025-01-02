51ba52d2ee038ace9d68c7190c052c4fe2208aa5
Tirumalareddy jaswanth kumar reddy <155978069+jaswanthkumarreddy999@users.noreply.github.com>
2025-01-02 23:34:14 +0530

    Add files via upload

---
+++ name.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class name {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int yourname = sc.nextInt();
+        System.out.println("your name is "+yourname);
+    }
+}
