15d50bb07429daa5cad7e10f0419a3eae0495a00
Tirumalareddy jaswanth kumar reddy <155978069+jaswanthkumarreddy999@users.noreply.github.com>
2025-01-03 22:41:45 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class add {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a+b);
+        float c = sc.nextFloat();
+        float d = sc.nextFloat();
+        System.out.println(c+d);
+    }
+}
