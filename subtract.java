8a9f31893e9716dfca27fa3ae63efc5e48694713
Tirumalareddy jaswanth kumar reddy <155978069+jaswanthkumarreddy999@users.noreply.github.com>
2025-01-04 11:03:25 +0530

    Add files via upload

---
+++ subtract.java
@@ -0,0 +1,10 @@
+public class subtract {
+    public static void main(String[] args) {
+        System.out.println();
+        int a = 9;
+        int b = 68;
+        float c = 70.81f;
+        float d = 0.81f;
+        System.out.println("result "+(c-d));
+    }
+}
