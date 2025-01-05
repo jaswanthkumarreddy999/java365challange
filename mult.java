a21b2b0d50aa03a56174a7cad8c43b56f8114859
Tirumalareddy jaswanth kumar reddy <155978069+jaswanthkumarreddy999@users.noreply.github.com>
2025-01-05 18:44:27 +0530

    Add files via upload

---
+++ mult.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class mult {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println();
+        double e = sc.nextDouble();
+        double f = sc.nextDouble();
+        System.out.println("result "+(e*f));
+        sc.close();
+    }
+}
