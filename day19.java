0e651d9fd1155453a55a9e5a4f3bc6b85255179b
Tirumalareddy jaswanth kumar reddy <155978069+jaswanthkumarreddy999@users.noreply.github.com>
2025-02-05 09:22:42 +0530

    Update and rename DAY19.java to day19.java

--- day19.java
+++ day19.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class DAY19 {
+public class day19 {
   public static void main(String[] args) {
     System.out.println();
     Scanner sc = new Scanner(System.in);
